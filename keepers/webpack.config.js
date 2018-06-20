ac91e33743d73cf2f7d7e52b989231ce84e6b597
Sierra Kilo <sierrakilo@Sierras-MBP.DFMNET01>
2018-06-20 10:45:35 -0700

    updated favicon info

--- keepers/webpack.config.js
+++ keepers/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      favicon: './public/favicon.ico'
+      favicon: './public/keepers.png'
     })
   ]
 };
