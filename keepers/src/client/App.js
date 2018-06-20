cd0324b795d1dfbb0f51e0b73739123d31d475cd
Sierra Kilo <sierrakilo@Sierras-MBP.DFMNET01>
2018-06-20 10:43:31 -0700

    removed boilerplate items not pertaining to project

--- keepers/src/client/App.js
+++ keepers/src/client/App.js
@@ -4,23 +4,13 @@ import './app.css';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: null };
-  }
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    // this.state = {  };
   }
 
   render() {
     return (
       <div>
-        {this.state.username ? (
-          <h1>Hello {this.state.username}</h1>
-        ) : (
-          <h1>Loading.. please wait!</h1>
-        )}
+          <h1>Getting Started</h1>
       </div>
     );
   }
