63eade0abab0a8e8f8ec03699e66a2c4c4eb0d7c
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-06-24 15:43:26 -0700

    added sequelize, and parser to server index.js

--- keepers/src/server/index.js
+++ keepers/src/server/index.js
@@ -1,8 +1,38 @@
+// const express = require('express');
+//
+// const app = express();
+//
+// // here from boilerplate, dont know what its for.
+// app.use(express.static('dist'));
+//
+// app.listen(8080, () => console.log('Listening on port 8080!'));
+
+
 const express = require('express');
+const bodyParser = require('body-parser')
+let PORT = process.env.PORT || 8080;
 
 const app = express();
 
-// here from boilerplate, dont know what its for.
-app.use(express.static('dist'));
+//Models
+const db = require("./models");
 
-app.listen(8080, () => console.log('Listening on port 8080!'));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Sync Database
+db.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+
+// app.use(express.static('dist'));
+
+//routes
+require('./routes/api-routes.js')(app)
+
+app.listen(PORT, () => console.log('Listening on port: ' + PORT));
