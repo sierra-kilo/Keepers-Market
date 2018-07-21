dba1a844324744e47b1e66bf68093cd99a918141
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-07-21 16:04:11 -0700

    added intro to main card, created intro component

---
+++ src/client/components/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Intro = () => (
+  <div>
+    <h5>About</h5>
+    <p>Welcome to Keepers Maket where you can buy whatever we sell.</p>
+    <p>Please sign up for our newsletter below to stay up to date with our goodies, and pop up locations.</p>
+    <p>Thank you</p>
+  </div>
+)
+
+export default Intro;
